7009dfc8551e2ce9e82bb7364dc03c3ab6ba9b52
FR8803 <61582118+FR8803@users.noreply.github.com>
2023-06-17 09:57:34 +0200

    Adding more detailed comments to the code

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,18 @@
-import os
+#Importing the necessary modules
 import logging
 from flask import Flask, render_template, request, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import json
 
+#Creating the flask application
 app = Flask(__name__)
 
 #Configuring logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-#Configuration
+#Defining the path to the relevant data files
 customer_data_path = '/Users/fabioza/Desktop/FinTech/MVP/customer_data.json'
 agent_chatcorpus_path = '/Users/fabioza/Desktop/FinTech/MVP/agent_chatcorpus.json'
 customer_chatcorpus_path = '/Users/fabioza/Desktop/FinTech/MVP/customer_chatcorpus.json'
@@ -20,40 +21,43 @@ customer_chatcorpus_path = '/Users/fabioza/Desktop/FinTech/MVP/customer_chatcorp
 with open(customer_data_path) as f:
     customer_data = json.load(f)
 
+#Setting global variables that are later used in load_spacy_model and train_chatbot
 nlp = None
 keywords = None
 responses = None
 
+#Function to load the spacy model lazily (the model is only imported and loaded when the function is called)
 def load_spacy_model():
     global nlp
     import spacy
     nlp = spacy.load("en_core_web_sm")
 
+#Training the two chatbots
 def train_chatbot(agent_chatbot, customer_chatbot):
-    # Train the chatbots
     try:
-        #Technically there could be two different chatcorpus
+        #Technically there could be two different chatcorpus but in this case both are the same
         trainer = ChatterBotCorpusTrainer(agent_chatbot)
         trainer.train(agent_chatcorpus_path)
-
         trainer = ChatterBotCorpusTrainer(customer_chatbot)
         trainer.train(customer_chatcorpus_path)
+    #Sending an exception to the log if training fails
     except Exception as e:
         logger.error(f"An error occurred during training: {e}")
 
+#Creating the two chatbots
 def create_chatbot():
-    # Create a new chatbot for the agent
     agent_chatbot = ChatBot('AgentFinFluent')
-
-    # Create a new chatbot for the customer
     customer_chatbot = ChatBot('CustomerFinFluent')
-
     return agent_chatbot, customer_chatbot
 
 def init():
+    #Initializing dictionarires with keywords and responses, these will be used from the spacy model to link keywords to responses
     global keywords, responses
+    #Creating the two chatbots
     agent_chatbot, customer_chatbot = create_chatbot()
+    #Training the chatbots
     train_chatbot(agent_chatbot, customer_chatbot)
+    #Loading the spacy model
     load_spacy_model()
     #Defining key words for each questions
     keywords = {
@@ -81,34 +85,44 @@ def init():
         }
     return agent_chatbot, customer_chatbot
 
-
+#Initiliazing the two chatbots
 agent_chatbot, customer_chatbot = init()
 
+#Defining the routes for the web application
+#This defines the landing page, the corresponding html code can be found in select.html
 @app.route("/")
 def home():
     return render_template("select.html")
 
+#Defining the route for the agent chatbot
+#Defining the message that is shown first once the chat is started
 @app.route("/agent_chatbot")
 def agent_chatbot_page():
     bot_message = 'Hello! Please enter a client number to load customer information, or enter "None" to proceed without loading customer information.'
     return render_template("agent_chatbot.html", bot_message=bot_message)
 
+#Defining the route for the customer chatbot
+#Defining the message that is shown first once the chat is started
 @app.route("/customer_chatbot")
 def customer_chatbot_page():
-    return render_template("customer_chatbot.html")
+    bot_message = 'Hello, how can I assist you today?'
+    return render_template("customer_chatbot.html", bot_message = bot_message)
 
+#Setting up a route that responds to http GET requests on the get_agent_response path
 @app.route("/get_agent_response", methods=["GET"])
 def get_agent_bot_response():
+    #Collecting the message that was sent by the user
     user_text = request.args.get('msg')
+    #Logging the user text (logs can be accessed in the browsers console, usually by hitting F12)
     logger.info('Received agent message: %s', user_text)
 
-    # If the agent enters "None", return a special message
+    #Message if the user doesn't want any client information
     if user_text.lower() == "none":
         response = 'Proceeding without loading customer information.'
         return response
-    # Check if the user input is a client number
+    #If first user input is client number
     elif user_text in customer_data:
-        # If it is, return the corresponding customer data
+        #Returning corresponding customer data, then displaying the initital question
         customer_info = customer_data[user_text]
         customer_info['customer_id'] = user_text
         response = {
@@ -117,28 +131,33 @@ def get_agent_bot_response():
         }
         return jsonify(response)
     else:
-        # If it's not, proceed as normal
+        #Otherwise proceed as normal, by gathering the right response from the json
        response = {
         'response': str(agent_chatbot.get_response(user_text))
         }
     return jsonify(response)
 
+#Defining the route for response to the agent
 @app.route("/get_agent_response", methods=["POST"])
 def get_agent_response():
-    data = request.json  # Get the JSON data sent in the POST
+    #Accesing the JSON data sent in the POST
+    data = request.json 
+    #Extracting the user message from the json
     user_text = data.get('msg')
+     #Extracting customer data from the json, default to empty dict if not present
     customer_data = data.get('customer_data', {})  # Extract customer data, default to empty dict if not present
-    # Process customer_data if needed
 
+    #Getting the chatbot response based on the user request
     chatbot_response = agent_chatbot.get_response(user_text)
     
-    # If chatbot confidence is high enough, the chatbot's response is used
+    #If chatbot confidence is high enough, the chatbot's response is used (meaning that the exact question can be found in the chatcorpus)
     if chatbot_response.confidence > 0.5:
         response = str(chatbot_response)
-    # Else keyword matching with spacy is used
+    #Otherwise keyword matching with spacy is used
     else:
+        #Parsing the user input
         doc = nlp(user_text)
-        # Lemmatizing the user input
+        #Lemmatizing the user input (converting to stems, removing punctuation and stopwords)
         user_keywords = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
 
         # Counting how many of the previously defined key words can be found in the user request and assigning scores
@@ -148,24 +167,28 @@ def get_agent_response():
                 if keyword in values:
                     scores[key] += 1
 
-        # The best answer has the highest score (most keywords match)
+        #The best answer has the highest score (most keywords match)
         best_match = max(scores, key=scores.get)
 
-        # If no matching words are found
+        #If no matching words are found
         if scores[best_match] == 0:
             response = 'Sorry, I did not understand that. Could you please rephrase your question?'
+        #Otherwise the best match is used as response and sent to the frontend
         else:
             response = responses[best_match]
             agent_chatbot.append(user_text, response)
 
     return jsonify({"response": response})
 
+#Defining the route for the response to the customer
 @app.route("/get_customer_response", methods=["POST"])
 def get_customer_response():
+    #Accesing the JSON data sent in the POST
     data = request.json
+    #Extracting the user request
     user_text = data.get('msg')
+    #Getting a response for the usser request
     chatbot_response = customer_chatbot.get_response(user_text)
-    print(chatbot_response)
 
     #If chatbot confidence is high enough, the chatbot's response is used
     if chatbot_response.confidence > 0.5:  
@@ -189,6 +212,7 @@ def get_customer_response():
         #If no matching words are found
         if scores[best_match] == 0:
                 response = 'Sorry, I did not understand that. Could you please rephrase your question?'
+        #Else the best match is streamed to the front end
         else:
             response = responses[best_match]
     return jsonify({"response": response})
